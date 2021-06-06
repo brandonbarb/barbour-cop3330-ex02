429ee21a06fd1e1d70550567411b456514d23dab
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-06 14:43:33 -0400

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("What is the input string?");
+        Scanner input = new Scanner(System.in);
+        String string = input.next();
+        int length = string.length();
+        System.out.println(string + " has " + length + " characters.");
+    }
+}
